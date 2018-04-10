73d2e3988225e5e86b31c82c28b7865045145c08
EvaLoison <36153994+EvaLoison@users.noreply.github.com>
2018-04-10 16:15:39 +0200

    Add files via upload

---
+++ jeux_de_donnees_promotions.sql
@@ -0,0 +1,9 @@
+INSERT INTO PROMOTION
+(NOM_PROMOTION, DATE_DEBUT_PROMOTION, DATE_FIN_PROMOTION)
+
+VALUES 
+
+('soldes_ete', DATE("01/07/2018"), DATE("30/09/2018")), 
+('soldes_hiver', DATE"06/01/2019"), DATE("30/02/2019")),
+('week_end_folies', DATE"08/04/2018"), DATE("09/04/2018")),
+('special_jardin', DATE("10/5/2018"), DATE("18/05/2018"));
