188d51a66d738a00a2b7b577c487acbfd07d97b2
EvaLoison <36153994+EvaLoison@users.noreply.github.com>
2018-04-10 16:52:29 +0200

    Update jeux_de_donnees_promotions.sql

--- jeux_de_donnees_promotions.sql
+++ jeux_de_donnees_promotions.sql
@@ -4,6 +4,6 @@ INSERT INTO PROMOTION
 VALUES 
 
 ('soldes_ete', DATE("01/07/2018"), DATE("30/09/2018")), 
-('soldes_hiver', DATE"06/01/2019"), DATE("30/02/2019")),
-('week_end_folies', DATE"08/04/2018"), DATE("09/04/2018")),
-('special_jardin', DATE("10/5/2018"), DATE("18/05/2018"));
+('soldes_hiver', DATE("06/01/2019"), DATE("30/02/2019")),
+('week_end_folies', DATE("08/04/2018"), DATE("09/04/2018")),
+('special_jardin', DATE("10/05/2018"), DATE("18/05/2018"));
